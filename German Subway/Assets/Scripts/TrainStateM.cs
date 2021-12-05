c7395517457efe795862d34f3897c4c5fd18f175
nickfunkey <nickfunkey@gmail.com>
2021-12-05 07:21:00 -0500

    added the ToString calls
    Still no clue why everything is error code I looked at it for a while and everything looked fine

--- German Subway/Assets/Scripts/TrainStateM.cs
+++ German Subway/Assets/Scripts/TrainStateM.cs
@@ -1,40 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.Sprites;
 
 public class TrainStateM : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       //InvokeRepeating(trainStateMatchine, 0, 1.0);
-		
+        //InvokeRepeating(trainStateMatchine, 0, 1.0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-	public void trainStateMatchine(){
-		//this repusents the trains orientation eatch value is how the train is faceing
-		enum Orientation { North = 1, NorthEast = 2, SouthEast = 3, South = 4, SouthWest=5, NorthWest=6   };
-		//gets the train position on the XYZ 
-		Vector3Int trainPos = transform.position;
-		//will find the currentHex the train is on
-		Tile currentHex=TileMap.getTile(trainPos);
-		
+
+    public void TrainStateMatchine() {
+        //this repusents the trains orientation eatch value is how the train is faceing
+    enum Orientation { North = 1, NorthEast = 2, SouthEast = 3, South = 4, SouthWest = 5, NorthWest = 6 };
+    //gets the train position on the XYZ 
+    Vector3Int trainPos = transform.position;
+    //will find the currentHex the train is on
+    Tile currentHex = Tilemap.getTile(trainPos);
+
+
 		
 		//will change train Orientation beased on the hex the train is on
-		if(currentHex=Stright){
+		if(currentHex.ToString=="Green Hex"){
 				//do nothing
 			}
-			else if(currentHex=Right){
+			else if(currentHex.ToString=="RedHex"){
 				Orientation=Orientation+1;
 			}
-			else if(currentHex=Left){
+			else if(currentHex.ToString=="Yellow Hex"){
 				Orientation=Orientation-1;
 			}
+    
 		
 		
 		
@@ -77,3 +82,4 @@ public class TrainStateM : MonoBehaviour
 
 
 }
+
