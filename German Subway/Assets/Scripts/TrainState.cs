f09ef8b6b217731eb6d187d64c261fb2152f3dbc
Aqilah Almohsen <aalmohse@emich.edu>
2021-12-06 07:01:24 -0500

    Fix errors for train

--- German Subway/Assets/Scripts/TrainState.cs
+++ German Subway/Assets/Scripts/TrainState.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.Sprites;
+using System;
 
 public class TrainStateM : MonoBehaviour
 {
+    public object PassengerTrain { get; private set; }
+
     //this represents the trains orientation each value is how the train is faceing
-    enum Orientation { North = 1, NorthEast = 2, SouthEast = 3, South = 4, SouthWest = 5, NorthWest = 6 };
+    enum Orientation { North = 1, NorthEast = 2, SouthEast = 3, South = 4, SouthWest = 5, NorthWest = 6, East = 7, West = 8 };
 
     // Start is called before the first frame update
     void Start()
@@ -15,56 +18,56 @@ public class TrainStateM : MonoBehaviour
         //InvokeRepeating(trainStateMatchine, 0, 1.0);
 
         // Direction Variable
-        Orientation myOrientation;
-        myOrientation = Orientation.South;
+      //  Orientation myOrientation;
+      //  myOrientation = Orientation.South;
 
     }
-    /** 
+   
        Orientation ChangeOrientation(Orientation dir)
        {
-         // Get position of train
-           PassengerTrain.
+        // Get position of train
+        PassengerTrain.GetType();
            if (dir == Orientation.North)
                dir = Orientation.South;
            else if (dir == Orientation.NorthEast)
-               dir = Orientation.North;
+               dir = Orientation.North; 
            else if (dir == Orientation.East)
                dir = Orientation.West;
            else if (dir == Orientation.West)
                dir = Orientation.East;
 
            return dir;
-       }
-           **/
+       }     
     // Update is called once per frame
+ 
     void Update()
     {
 
     }
 
-    /**
-    public void TrainStateMachine()
-    {  
-        //gets the train position on the XYZ
-        Vector2 trainPos = transform.position;
-        
-        //will find the currentHex the train is on
-        Tile currentHex = Tilemap.getTile(trainPos);
-
-        //will change train Orientation based on the hex the train is on
-        if (currentHex.ToString == "Green Hex")
-        {
-            //do nothing
-        }
-        else if (currentHex.ToString == "RedHex")
-        {
-            Orientation = Orientation + 1;
-        }
-        else if (currentHex.ToString == "Yellow Hex")
-        {
-            Orientation = Orientation - 1;
-        }
-
+ 
+       public void TrainStateMachine()
+       {  
+           //gets the train position on the XYZ
+           Vector2 trainPos = transform.position;
+           
+           //will find the currentHex the train is on
+           Tile currentHex = Tilemap.getTile(trainPos);
+        /**
+            //will change train Orientation based on the hex the train is on
+            if (currentHex.ToString == "Green Hex")
+            {
+                //do nothing
+            }
+            else if (currentHex.ToString == "RedHex")
+            {
+                Orientation = Orientation + 1;
+            }
+            else if (currentHex.ToString == "Yellow Hex")
+            {
+                Orientation = Orientation - 1;
+            }
+     **/
         switch (Orientation)
         {
 
@@ -91,7 +94,35 @@ public class TrainStateM : MonoBehaviour
             case SouthEast:
                 moveSouthEast();
                 break;
-
+            default:
+                break;
         }
-    **/
+
+
+    }
+
+    private void moveSouthWest()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void moveSouth()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void moveSouthEast()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void moveNorthEast()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void moveNorth()
+    {
+        throw new NotImplementedException();
+    }
 }
