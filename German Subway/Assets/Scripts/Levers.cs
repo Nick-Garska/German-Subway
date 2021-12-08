5f2d778914596cb4d32814fa3d580880b43cbf66
halkafah <halkafah@emich.edu>
2021-12-07 21:12:11 -0500

    more lever code

--- German Subway/Assets/Scripts/Levers.cs
+++ German Subway/Assets/Scripts/Levers.cs
@@ -9,10 +9,14 @@ public class Levers : MonoBehaviour
 {
     public GameObject lever1;
     private bool lever1Open = true;
+    private float delay = 1;
     // Start is called before the first frame update
     void Start()
     {
-       lever1 = GameObject.Find("Lever");
+        lever1 = GameObject.Find("Lever");
+
+        //InvokeRepeating("LocateHex()", 1.0f, 5.0f);
+        StartCoroutine(LocateHex(delay));
     }
 
     // Update is called once per frame
@@ -20,15 +24,43 @@ public class Levers : MonoBehaviour
     {
         Vector3 trainPos = transform.position;
         //Tile currentHex = TileMap.getTile(trainPos);
-       // print("pos" + currentHex);
+        // print("pos" + currentHex);
         if (lever1Open)
         {
-           // SetTile(Vector3Int position, Tilemaps.TileBase tile);
+            // SetTile(Vector3Int position, Tilemaps.TileBase tile);
         }
     }
-}
 
-internal class TileMap
+    IEnumerator LocateHex(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        //ok this is an array of all things taged with the HexMap tag witch is just our one tilemap so tempholder[0] will return a refrence to our tilrmap
+        GameObject[] tempholder = GameObject.FindGameObjectsWithTag("HexMap");
+
+        //turns the Gameo
+        TileMap rails = tempholder[0].GetComponent<TileMap>();
+
+
+        //gets the train position on the XYZ
+        Vector3 trainPos = transform.position;
+
+
+        //will find the currentHex the train is on
+        Tile currentHex = GetCurrentHex(rails, trainPos);
+
+
+        print(currentHex + " " + trainPos);
+
+
+    }
+
+    private static Tile GetCurrentHex(TileMap rails, Vector3 trainPos)
+    {
+        return rails.GetTile(trainPos);
+    }
+}
+    internal class TileMap
 {
     internal static Tile getTile(Vector3 trainPos)
     {
